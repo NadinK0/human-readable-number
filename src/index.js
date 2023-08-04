f9bc02b9d8f692bc92f0107812c590d6648b0672
Nadejda Klinskaya <npantelyeva@gmail.com>
2023-08-04 16:49:02 +0300

    feat: add solution for human-readable-number task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function toReadable (number) {
-  
+  const m1_10 = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const m20_90 = ['twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  const m100 = ['hundred'];
+  let newArr = Array.from(String(number), Number);
+    let l = newArr.length;
+    if (l < 3 && number < 20 ) 
+    {
+      return m1_10[number];
+    } else if (l < 3)
+      {
+        newArr[newArr.length-1] === 0 ? m20_90[newArr[0]-2] : `${m20_90[newArr[0]-2]} ${m1_10[newArr[newArr.length-1]]}`;
+      } else if (l < 4) 
+        {
+          newArr[newArr.length-1] === 0 ? 
+    
+            newArr[newArr.length-2] === 0 ? 
+            `${m1_10[newArr[0]]} ${m100[0]}` : 
+            `${m1_10[newArr[0]]} ${m100[0]} ${m20_90[newArr[0]-2]}` : 
+    
+            newArr[newArr.length-2] === 0? 
+            `${m1_10[newArr[0]]} ${m100[0]} ${m1_10[newArr[newArr.length-1]]}`:
+            `${m1_10[newArr[0]]} ${m100[0]} ${m20_90[newArr[0]-2]} ${m1_10[newArr[newArr.length-1]]}`;
+        }
 }
