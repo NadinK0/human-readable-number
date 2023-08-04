d83a1d852f833a108accba91d50ba67e6f0468b1
Nadejda Klinskaya <npantelyeva@gmail.com>
2023-08-04 16:59:07 +0300

    fix: task solution

--- src/index.js
+++ src/index.js
@@ -4,22 +4,25 @@ module.exports = function toReadable (number) {
   const m100 = ['hundred'];
   let newArr = Array.from(String(number), Number);
     let l = newArr.length;
+    let readableNumber;
     if (l < 3 && number < 20 ) 
     {
-      return m1_10[number];
+      readableNumber = m1_10[number];
     } else if (l < 3)
       {
-        newArr[newArr.length-1] === 0 ? m20_90[newArr[0]-2] : `${m20_90[newArr[0]-2]} ${m1_10[newArr[newArr.length-1]]}`;
+        newArr[newArr.length-1] === 0 ? readableNumber = m20_90[newArr[0]-2] : readableNumber = `${m20_90[newArr[0]-2]} ${m1_10[newArr[newArr.length-1]]}`;
       } else if (l < 4) 
         {
           newArr[newArr.length-1] === 0 ? 
     
             newArr[newArr.length-2] === 0 ? 
-            `${m1_10[newArr[0]]} ${m100[0]}` : 
-            `${m1_10[newArr[0]]} ${m100[0]} ${m20_90[newArr[0]-2]}` : 
+            readableNumber = `${m1_10[newArr[0]]} ${m100[0]}` : 
+            readableNumber = `${m1_10[newArr[0]]} ${m100[0]} ${m20_90[newArr[0]-2]}` : 
     
             newArr[newArr.length-2] === 0? 
-            `${m1_10[newArr[0]]} ${m100[0]} ${m1_10[newArr[newArr.length-1]]}`:
-            `${m1_10[newArr[0]]} ${m100[0]} ${m20_90[newArr[0]-2]} ${m1_10[newArr[newArr.length-1]]}`;
+            readableNumber = `${m1_10[newArr[0]]} ${m100[0]} ${m1_10[newArr[newArr.length-1]]}`:
+            readableNumber = `${m1_10[newArr[0]]} ${m100[0]} ${m20_90[newArr[0]-2]} ${m1_10[newArr[newArr.length-1]]}`;
         }
+
+        return readableNumber;
 }
