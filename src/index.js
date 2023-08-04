e06102e9efcf5508c3271cd7bc621932d049e2ef
Nadejda Klinskaya <npantelyeva@gmail.com>
2023-08-04 17:07:54 +0300

    fix: task solution

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ module.exports = function toReadable (number) {
     
             newArr[newArr.length-2] === 0? 
             readableNumber = `${m1_10[newArr[0]]} ${m100[0]} ${m1_10[newArr[newArr.length-1]]}`:
-            readableNumber = `${m1_10[newArr[0]]} ${m100[0]} ${m20_90[newArr[0]-2]} ${m1_10[newArr[newArr.length-1]]}`;
+            readableNumber = `${m1_10[newArr[0]]} ${m100[0]} ${m20_90[newArr[1]-2]} ${m1_10[newArr[newArr.length-1]]}`;
         }
 
         return readableNumber;
