b613a7feca63ebd07659f95090968fab4e3776a2
Nadejda Klinskaya <npantelyeva@gmail.com>
2023-08-04 17:29:43 +0300

    fix: correct error in solution

--- src/index.js
+++ src/index.js
@@ -19,10 +19,13 @@ module.exports = function toReadable (number) {
             readableNumber = `${m1_10[newArr[0]]} ${m100[0]}` : 
             readableNumber = `${m1_10[newArr[0]]} ${m100[0]} ${m20_90[newArr[1]-2]}` : 
     
-            newArr[newArr.length-2] === 0? 
-            readableNumber = `${m1_10[newArr[0]]} ${m100[0]} ${m1_10[newArr[newArr.length-1]]}`:
-            readableNumber = `${m1_10[newArr[0]]} ${m100[0]} ${m20_90[newArr[1]-2]} ${m1_10[newArr[newArr.length-1]]}`;
-        }
+            newArr[newArr.length-2] === 0 ? 
 
+              readableNumber = `${m1_10[newArr[0]]} ${m100[0]} ${m1_10[newArr[newArr.length-1]]}`:
+              newArr[newArr.length-2] === 1 ?
+              readableNumber = `${m1_10[newArr[0]]} ${m100[0]} ${m1_10[`${newArr[1]}${newArr[2]}`]}`:
+              readableNumber = `${m1_10[newArr[0]]} ${m100[0]} ${m20_90[newArr[1]-2]} ${m1_10[newArr[newArr.length-1]]}`;
+          }
+          
         return readableNumber;
 }
